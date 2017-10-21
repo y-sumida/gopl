e6b628d6182322d32e698888b97ec79bcd729a0d
Yuki Sumida <y.sumida.0201@gmail.com>
2017-10-21 22:30:46 +0900

    ch4.5 movie unmarshal

--- ch4/movie.go
+++ ch4/movie.go
@@ -31,4 +31,10 @@ func main() {
 		log.Fatalf("JSON marshaling failed: %s", err)
 	}
 	fmt.Printf("%s\n", data)
+
+	var titles []struct{ Title string }
+	if err = json.Unmarshal(data, &titles); err != nil {
+		log.Fatalf("JSON unmarshaling failed: %s", err)
+	}
+	fmt.Println(titles)
 }
