58b4ede24cd96cac794e6a222748fc1e81bd90cd
Yuki Sumida <y.sumida.0201@gmail.com>
2017-10-23 21:56:44 +0900

    ch4.5 github 4

--- ch4/issues.go
+++ ch4/issues.go
@@ -13,4 +13,8 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("%d issues:\n", result.TotalCount)
+
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	}
 }
