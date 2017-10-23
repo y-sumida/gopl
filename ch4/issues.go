527d838cb92106badb423081c1fe71448d4a019d
Yuki Sumida <y.sumida.0201@gmail.com>
2017-10-23 21:53:12 +0900

    ch4.5 github 3

---
+++ ch4/issues.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"gopl/ch4/github"
+)
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues:\n", result.TotalCount)
+}
