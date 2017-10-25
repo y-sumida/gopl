a63cc9b964ccc1f6121e89f882235497f4ff650b
Yuki Sumida <y.sumida.0201@gmail.com>
2017-10-25 23:36:40 +0900

    exercise4.10 2

--- ch4/issues.go
+++ ch4/issues.go
@@ -16,25 +16,27 @@ func main() {
 	fmt.Printf("%d issues:\n", result.TotalCount)
 
 	now := time.Now()
+	monthAgo := now.AddDate(0, -1, 0)
+	yearAgo := now.AddDate(-1, 0, 0)
 
-	fmt.Println("1ヶ月未満")
+	fmt.Printf("\n%s 以後\n", monthAgo.Format("2006-01-02"))
 	for _, item := range result.Items {
-		if now.Sub(item.CreatedAt).Hours()/24 <= 30 {
-			fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+		if item.CreatedAt.After(monthAgo) {
+			fmt.Printf("#%-5d %9.9s %.55s %s\n", item.Number, item.User.Login, item.Title, item.CreatedAt)
 		}
 	}
 
-	fmt.Println("1年未満")
+	fmt.Printf("\n%s 以後\n", yearAgo.Format("2006-01-02"))
 	for _, item := range result.Items {
-		if now.Sub(item.CreatedAt).Hours()/24 <= 365 {
-			fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+		if item.CreatedAt.After(yearAgo) {
+			fmt.Printf("#%-5d %9.9s %.55s %s\n", item.Number, item.User.Login, item.Title, item.CreatedAt)
 		}
 	}
 
-	fmt.Println("1年以上")
+	fmt.Printf("\n%s 以前\n", yearAgo.Format("2006-01-02"))
 	for _, item := range result.Items {
-		if now.Sub(item.CreatedAt).Hours()/24 > 365 {
-			fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+		if item.CreatedAt.Before(yearAgo) {
+			fmt.Printf("#%-5d %9.9s %.55s %s\n", item.Number, item.User.Login, item.Title, item.CreatedAt)
 		}
 	}
 }
